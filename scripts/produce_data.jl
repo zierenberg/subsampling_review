69dab78ae9eb9735031f7090b27e2b5e30b3d24e
Johannes Zierenberg <johannes.zierenberg@gmail.com>
2022-02-17 10:31:41 +0100

    added documentation

--- scripts/produce_data.jl
+++ scripts/produce_data.jl
@@ -24,16 +24,6 @@ boundary conditions. Parameter can be specified if desired.
 Model is Abelian sandpile plus periodic boundary conditions and the chance
 that toppling does not propagate grains (to make it subcritical)
 
-In fact, check the definition in
-
-[1]S. N. Majumdar and D. Dhar, Physica A: Statistical Mechanics and Its Applications 185, 129 (1992).
-
-for abelian sandpile models (ABS) on periodic boundary conditions where the
-dissipation is implemented via a sink to the system (symmetric and solves some
-disambiguities)
-
-For now stick to the solution by Levina and Priesemann, but let's keep thinking about an ambiguous solution
-
 L=4;
 Ls=(L,L);
 seed=1000;
@@ -85,8 +75,7 @@ function produce_data_avalanches_sandpile(;
     progress = Progress(num_avalanches, 1, "sampling: ", offset=0)
     while num_valid_avalanches < num_avalanches
         push!(data["mean_h"], mean(hs))
-        # find smart way to check for stationary critical dynamics from state
-        # hs (Dhar?) (do this after every 10 non-zero avalanches or so)
+        # get list of lists of toppled sites (one list of toppled sites per time step)
         topple_sites_over_time = perturb_and_relax(rng, hs, lattice, hc, p_dis)
         ProgressMeter.update!(progress, num_valid_avalanches)
         if length(topple_sites_over_time) > 0
@@ -123,74 +112,74 @@ function produce_data_avalanches_sandpile(;
 
     return subsample, data
 end
-function produce_data_perturb_relax_bn(;
-        ref=4,
-        L::Int=Int(2^7), # linear extension of lattice
-        Ls=(L,L),          # 1D lattice
-        p_rec::Float64=0.644, #
-        seed=1000,  # seed for random number generator
-        num_avalanches::Int = Int(1e5),
-        periodic=true
-    )
-    rng = MersenneTwister(seed)
-
-    lattice = grid(Ls, periodic=periodic)
-    state = zeros(Bool, Ls)
-    state_ref = zeros(Int, Ls)
-    state_new = zeros(Bool, Ls)
-
-    subsample = Dict{String, Any}("l" => Int[2^2, 2^4, 2^6])
-    subsample["random"] = [Vector{Int}(undef,l*l) for l in subsample["l"]]
-    subsample["window"] = [Vector{Int}(undef,l*l) for l in subsample["l"]]
-    # fill the subsampling schemes
-    for (i,l) in enumerate(subsample["l"])
-        subsample["random"][i] .= sample(rng, vertices(lattice), length(subsample["random"][i]), replace=false)
-        range =  floor(Int,L/2)-floor(Int,l/2)+1 : floor(Int,L/2)-floor(Int,l/2)+l
-        subsample["window"][i] .= vcat(LinearIndices(size(state))[range, range]...)
-    end
-
-    num_valid_avalanches = 0
-    data = Dict()
-    data["duration"] = Dict{String,Any}()
-    data["duration"]["full"] = zeros(Int64, num_avalanches)
-
-    data["size"] = Dict{String,Any}()
-    data["size"]["full"] = zeros(Int64, num_avalanches)
-    data["size"]["random"] = [zeros(Int64, num_avalanches) for i in 1:length(subsample["random"])]
-    data["size"]["window"] = [zeros(Int64, num_avalanches) for i in 1:length(subsample["window"])]
-
-    progress = Progress(num_avalanches, 1, "sampling: ", offset=0)
-    for num_valid_avalanches in 1:num_avalanches
-        next!(progress)
-
-        # single perturbation
-        state_ref .= 0
-        state[rand(rng, vertices(lattice))] = true
-        state_ref[state] .= ref
-        activity = sum(state)
-        while !(activity == 0)
-            # full sampling
-            data["duration"]["full"][num_valid_avalanches] += 1
-            data["size"]["full"][num_valid_avalanches] += activity
-            # random spatial subsampling
-            for (i, test_sample) in enumerate(subsample["random"])
-                a_t = sum(state[test_sample])
-                data["size"]["random"][i][num_valid_avalanches] += a_t
-            end
-
-            # field-of-view spatial subsampling
-            for (i, test_sample) in enumerate(subsample["window"])
-                a_t = sum(state[test_sample])
-                data["size"]["window"][i][num_valid_avalanches] += a_t
-            end
-            # next step
-            step!(rng, state, state_new, state_ref, ref, lattice, p_rec)
-            activity = sum(state)
-        end
-    end
-
-    return data
-end
+#function produce_data_perturb_relax_bn(;
+#        ref=4,
+#        L::Int=Int(2^7), # linear extension of lattice
+#        Ls=(L,L),          # 1D lattice
+#        p_rec::Float64=0.644, #
+#        seed=1000,  # seed for random number generator
+#        num_avalanches::Int = Int(1e5),
+#        periodic=true
+#    )
+#    rng = MersenneTwister(seed)
+#
+#    lattice = grid(Ls, periodic=periodic)
+#    state = zeros(Bool, Ls)
+#    state_ref = zeros(Int, Ls)
+#    state_new = zeros(Bool, Ls)
+#
+#    subsample = Dict{String, Any}("l" => Int[2^2, 2^4, 2^6])
+#    subsample["random"] = [Vector{Int}(undef,l*l) for l in subsample["l"]]
+#    subsample["window"] = [Vector{Int}(undef,l*l) for l in subsample["l"]]
+#    # fill the subsampling schemes
+#    for (i,l) in enumerate(subsample["l"])
+#        subsample["random"][i] .= sample(rng, vertices(lattice), length(subsample["random"][i]), replace=false)
+#        range =  floor(Int,L/2)-floor(Int,l/2)+1 : floor(Int,L/2)-floor(Int,l/2)+l
+#        subsample["window"][i] .= vcat(LinearIndices(size(state))[range, range]...)
+#    end
+#
+#    num_valid_avalanches = 0
+#    data = Dict()
+#    data["duration"] = Dict{String,Any}()
+#    data["duration"]["full"] = zeros(Int64, num_avalanches)
+#
+#    data["size"] = Dict{String,Any}()
+#    data["size"]["full"] = zeros(Int64, num_avalanches)
+#    data["size"]["random"] = [zeros(Int64, num_avalanches) for i in 1:length(subsample["random"])]
+#    data["size"]["window"] = [zeros(Int64, num_avalanches) for i in 1:length(subsample["window"])]
+#
+#    progress = Progress(num_avalanches, 1, "sampling: ", offset=0)
+#    for num_valid_avalanches in 1:num_avalanches
+#        next!(progress)
+#
+#        # single perturbation
+#        state_ref .= 0
+#        state[rand(rng, vertices(lattice))] = true
+#        state_ref[state] .= ref
+#        activity = sum(state)
+#        while !(activity == 0)
+#            # full sampling
+#            data["duration"]["full"][num_valid_avalanches] += 1
+#            data["size"]["full"][num_valid_avalanches] += activity
+#            # random spatial subsampling
+#            for (i, test_sample) in enumerate(subsample["random"])
+#                a_t = sum(state[test_sample])
+#                data["size"]["random"][i][num_valid_avalanches] += a_t
+#            end
+#
+#            # field-of-view spatial subsampling
+#            for (i, test_sample) in enumerate(subsample["window"])
+#                a_t = sum(state[test_sample])
+#                data["size"]["window"][i][num_valid_avalanches] += a_t
+#            end
+#            # next step
+#            step!(rng, state, state_new, state_ref, ref, lattice, p_rec)
+#            activity = sum(state)
+#        end
+#    end
+#
+#    return data
+#end
 
 
 """
